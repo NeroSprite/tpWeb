8f63e27c8acf94e5620c5c4db1e1af88dcda27b1
AntoineDegas <antdegas@gmail.com>
2021-02-19 09:42:00 +0100

    Q2-Q5

--- interaction.js
+++ interaction.js
@@ -3,15 +3,41 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
-  var startX;
-  var startY;
-  var endX;
-  var endY;
+  this.startX;
+  this.startY;
+  this.endX;
+  this.endY;
+  this.pressed;
 
 
 	// Developper les 3 fonctions gérant les événements
+  DnD.prototype.Press = function (evt){
+    this.startX = evt.x;
+    this.startY = evt.y;
+    this.pressed = true;
+    console.log("Start : " + this.startX + " : " + this.startY);
+  }
+
+  DnD.prototype.Move = function (evt){
+    if(this.pressed === true){
+      this.endX = evt.x;
+      this.endY = evt.y;
+    }
+  }
+
+  DnD.prototype.Release = function (evt){
+    this.endX = evt.x;
+    this.endY = evt.y;
+    this.pressed = false;
+    console.log("Stop : " + this.endX + " : " + this.endY);
+  }
+
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.Press, false);
+  canvas.addEventListener('mousemove', this.Move, false);
+  canvas.addEventListener('mouseup', this.Release, false);
+
 };
 
 
