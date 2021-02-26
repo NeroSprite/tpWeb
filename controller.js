62c0413cdfba4fb6496526ef251fc664b7acd96a
AntoineDegas <antdegas@gmail.com>
2021-02-26 11:56:52 +0100

    Q8 Fix widgets de la classe controller / Q9 onInteractionStart v0

--- controller.js
+++ controller.js
@@ -10,26 +10,44 @@ function Pencil(ctx, drawing, canvas) {
 	// Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
 	document.getElementById('colour').addEventListener("change", function (){
 		this.currColour = document.getElementById('colour').value;
-	}.bind(this));
+		console.log("New color : " + this.currColour);
+	}.bind(this), false);
+
+	document.getElementById('spinnerWidth').addEventListener("change", function (){
+		this.currLineWidth = document.getElementById('spinnerWidth').value;
+		console.log("New line width : " + this.currLineWidth);
+	}.bind(this), false);
 
 
 	document.getElementById('butRect').addEventListener("click", function () {
-		this.currEditingMode = editingMode.rect;
+		this.currEditingMode = 0;
 		console.log("Bouton Rect");
-	}.bind(this));
+	}.bind(this) , false);
 
 	document.getElementById('butLine').addEventListener("click", function () {
-		this.currEditingMode = editingMode.line;
+		this.currEditingMode = 1;
 		console.log("Bouton Line");
-	}.bind(this));
+	}.bind(this) , false);
 
 
 	new DnD(canvas, this);
 
-	onInteractionEnd = function(){
-
-	}
 	// Implémentez ici les 3 fonctions onInteractionStart, onInteractionUpdate et onInteractionEnd
+	this.onInteractionStart = function( myDND ) {
+		this.currLineWidth = document.getElementById('spinnerWidth').value; // taille du trait
+		this.currColour = document.getElementById('colour').value;
+		if(this.currentShape === editingMode.rect ) {
+			console.log("Start creation of a Rectangle...");
+		}
+		else {
+			console.log("Now we do a Line ...");
+		}
+	}
+
+
+
+
+
 };
 
 
